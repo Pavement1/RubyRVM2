ba1f12176dff38633d3c15775cbe8bbd27508355
Pavement1 <joakim.hals@gmail.com>
2015-05-11 22:03:49 +0200

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,10 +3,16 @@ class ArticlesController < ApplicationController
 	end
 	
 	def create
-		@article = Article.new(params[:article])
+		@article = Article.new(params.require(:article).permit(:title, :text))
 		
 		@article.save
 		redirect_to @article
+	end
+	
+	private
+		def article_params
+			params.require(:article).permit(:title, :text)
+		end
 	end
 end
 
