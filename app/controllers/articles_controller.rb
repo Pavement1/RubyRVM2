ec882ce5127e130c011d1c34ab08518645419615
Pavement1 <joakim.hals@gmail.com>
2015-05-12 13:45:44 +0200

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,8 +15,9 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 	
-	#def edit
-	#	@article = Article.new(article_params)
+	def edit
+		@article = Article.new(article_params)
+	end
 	
 	def create
 		@article = Article.new(article_params)
