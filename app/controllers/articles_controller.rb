3af7ca28a7f0b0f65e4cc249ad7575d42e44ce69
Pavement1 <joakim.hals@gmail.com>
2015-05-12 13:38:00 +0200

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,11 @@
 class ArticlesController < ApplicationController
+
+	private
+		def article_params
+			params.require(:article).permit(:title, :text)
+		end
+	end
+
 	def index
 		@articles = Article.all
 	end
@@ -34,9 +41,5 @@ class ArticlesController < ApplicationController
 		end
 	end
 
-	private
-		def article_params
-			params.require(:article).permit(:title, :text)
-		end
-	end
+
 end
